755637670b3740cbd1a605bdba3dc714b66a36c2
abdulhafis786 <44540208+abdulhafis786@users.noreply.github.com>
2020-08-06 11:24:02 +0530

    Updating comments

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,4 +4,5 @@ echo "************************"
 echo "******BUILDING JAR******"
 echo "************************"
 
+#Here the full path of the Jenkins workspace is given as the Git repo is downloaded to the workspace loacation
 docker run --rm -v /root/jenkins-data/jenkins_home/workspace/pipeline-docker-maven/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
