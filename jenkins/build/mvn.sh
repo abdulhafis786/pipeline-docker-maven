a7ab2d3a1c6e5c46c4cb258410b0a9e007bb4d33
root <root@ip-172-31-43-99.ap-south-1.compute.internal>
2020-08-06 05:47:56 +0000

    Fixed the changes for mvn scripts

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,4 +4,4 @@ echo "************************"
 echo "******BUILDING JAR******"
 echo "************************"
 
-docker run --rm -v $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+docker run --rm -v /root/jenkins-data/jenkins_home/workspace/pipeline-docker-maven/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
